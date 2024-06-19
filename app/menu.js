50a41d59deadba68f97dfcf6487cebd4b142f4ba
unknown <gerlleandrosyllva@gmail.com>
2024-06-18 22:59:44 -0300

    second commit

---
+++ app/menu.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const icon = document.getElementById('icon');
+    const menuToggle = document.getElementById('menu-toggle');
+    const navbarMenu = document.getElementById('navbarNavDropdown'); 
+
+    // Mostrar/Esconder o menu
+    icon.addEventListener('click', () => {
+        
+    })
+});
